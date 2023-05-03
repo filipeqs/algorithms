a746830971562fce37ef941fce17db5826b7af7c
Filipe Silva <filipeqs@outlook.com>
2023-05-02 22:37:47 -0400

    bubble sort

---
+++ JavaScript/Sort/bubbleSort.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+  for (let i = arr.length; i >= 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      const current = arr[j];
+      const next = arr[j + 1];
+
+      if (current > next) {
+        arr[j] = next;
+        arr[j + 1] = current;
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([5, 3, 4, 1, 2]));
